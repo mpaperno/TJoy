495f2648e086787f97b24b427cc72bd332e42566
Max Paperno <max@wdg.us>
2022-03-18 03:49:14 -0400

    [vGen] Use static version numbers and adjust AssemblyInfo for wrapper.

--- src/vGen/vGenWrap/Properties/AssemblyInfo.cs
+++ src/vGen/vGenWrap/Properties/AssemblyInfo.cs
@@ -1,36 +1,28 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("vJoyInterfaceWrap")]
-[assembly: AssemblyDescription("C# Wrapper for vJoyInterface.dll")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("C# Wrapper for vGenInterface.dll")]
 [assembly: AssemblyCompany("Shaul Eizikovich")]
-[assembly: AssemblyProduct("vJoyInterfaceWrap")]
-[assembly: AssemblyCopyright("Copyright Shaul Eizikovich ©  2015")]
+[assembly: AssemblyProduct("vGenInterfaceWrap")]
+[assembly: AssemblyCopyright("Copyright Shaul Eizikovich ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("30750eab-8093-4385-bee2-a438fcebc20e")]
+[assembly: Guid("441AA28A-3FB9-4523-AAA4-FF9E322BC7D2")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.1.5")]
-[assembly: AssemblyFileVersion("0.2.1.5")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyTitle("vGenInterfaceWrap  - C# Wrapper [Deb]")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyTitle("vGenInterfaceWrap  - C# Wrapper [Rel]")]
+#endif
---
+++ src/vGen/vGenWrap/Properties/AssemblyVersionInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Revision
+//      Build
+[assembly: AssemblyInformationalVersion("1.0.1.2-mp")]
+[assembly: AssemblyVersion("1.0.1.2")]
+[assembly: AssemblyFileVersion("1.0.1.2")]
