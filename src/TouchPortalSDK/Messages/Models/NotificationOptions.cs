c6ef6ba11c1c3bd587460f2153f917709cc922e2
Max Paperno <max@wdg.us>
2022-07-12 11:09:34 -0400

    Remove TouchPortalSDK project and use nuget version instead.

--- src/TouchPortalSDK/Messages/Models/NotificationOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TouchPortalSDK.Messages.Models
-{
-    public class NotificationOptions
-    {
-        public string Id { get; set; }
-
-        public string Title { get; set; }
-    }
-}
--- src/TouchPortalSDK/TouchPortalOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TouchPortalSDK
-{
-    public class TouchPortalOptions
-    {
-        public string IpAddress { get; set; } = "127.0.0.1";
-        public int Port { get; set; } = 12136;
-        public static char ActionDataIdSeparator { get; set; } = '\0';
-    }
-}
