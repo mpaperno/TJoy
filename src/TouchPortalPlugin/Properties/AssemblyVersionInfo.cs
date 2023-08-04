c6ba97d9f02609e55e7c1a11c1e742f5f10f1299
Max Paperno <max@wdg.us>
2023-08-04 13:06:26 -0400

    [Plugin] Update project config file and bump version to 1.1.0.0.

--- src/TouchPortalPlugin/Properties/AssemblyVersionInfo.cs
+++ src/TouchPortalPlugin/Properties/AssemblyVersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyInformationalVersion("1.0.0.1-beta1")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyInformationalVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
